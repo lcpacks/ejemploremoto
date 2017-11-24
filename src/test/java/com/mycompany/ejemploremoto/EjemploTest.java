7d663a5a68215dbbd45d4aef313f3206b42e2f2f
paco <paco@xyz.com>
2017-11-24 09:46:22 +0100

    cambio de prueba

--- src/test/java/com/mycompany/ejemploremoto/EjemploTest.java
+++ src/test/java/com/mycompany/ejemploremoto/EjemploTest.java
@@ -1,17 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.ejemploremoto;
 
 import java.util.List;
+import java.util.logging.Logger;
+import static java.util.logging.Logger.getLogger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
@@ -19,21 +16,38 @@ import static org.junit.Assert.*;
  */
 public class EjemploTest {
     
-    public EjemploTest() {
-    }
+    private static final Logger LOG = getLogger(EjemploTest.class.getName());
     
+    /**
+     *
+     */
     @BeforeClass
     public static void setUpClass() {
     }
     
+    /**
+     *
+     */
     @AfterClass
     public static void tearDownClass() {
     }
+
+    /**
+     *
+     */
+    public EjemploTest() {
+    }
     
+    /**
+     *
+     */
     @Before
     public void setUp() {
     }
     
+    /**
+     *
+     */
     @After
     public void tearDown() {
     }
@@ -42,6 +56,11 @@ public class EjemploTest {
     Como un usuario, quiero poder añadir una película
 	a mi lista de películas
 */
+
+    /**
+     *
+     */
+
      @Test
      public void annadirPeliculaTest() {
          Usuario u = new Usuario();
